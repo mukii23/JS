4ca1a579d3d478d0324fdda4065de877c301ef76
Mukii <mukii2307@gmail.com>
2018-10-23 11:41:09 +0530

    Create Other.js
    - Limit the paragraph content.

---
+++ Other.js
@@ -0,0 +1,15 @@
+/***************
+**** Show '...' after 250 words in Paragraph ****
+***************/
+
+$('.class').each(function() {
+         var showChar = 250;
+         var content = $(this).html();
+
+         if(content.length > showChar) {
+                var c = content.substr(0, showChar);
+                var h = content.substr(showChar-1, content.length - showChar);
+                var html = c + '...';
+                $(this).html(html);
+           }
+ });
